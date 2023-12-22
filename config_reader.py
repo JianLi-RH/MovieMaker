00f2bef78b3c811c79adef99bac430f2244bdfdc
Jian Li <jianl@redhat.com>
2023-12-22 20:11:08 +0800

    将配置拆分成全局和个人两种

--- config_reader.py
+++ config_reader.py
@@ -1,14 +1,19 @@
 import yaml
 
-config = yaml.safe_load(open("config.yaml"))
+***REMOVED*** Global settings
+config = yaml.safe_load(open("global_config.yaml"))
+output_dir = config["output_dir"]
+sucai_dir = config["sucai_dir"]
+system_font_dir = config["system_font_dir"]
+font = config["font"]
+video_format = ".mp4"
 
+
+***REMOVED*** Personal settings
+config = yaml.safe_load(open("config.yaml"))
 fps = int(config["fps"])    ***REMOVED*** 每秒显示的帧数
 watermark = config["watermark"] ***REMOVED*** 水印
 g_width = int(config["g_width"])
 g_height = int(config["g_height"])
-output_dir = config["output_dir"]
-sucai_dir = config["sucai_dir"]
 round_per_second = config["round_per_second"]
-font = config["font"]
 font_size = int(config["font_size"])
-system_font_dir = config["system_font_dir"]
