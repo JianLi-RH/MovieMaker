ef85f3f43cde0403ff5def0c0304bc2052096d6c
Jian Li <jianl@redhat.com>
2024-10-30 22:05:37 +0800

    优化了拆分音频文件的方法, 可以任意对start和length使用初始值了

--- libs/AudioHelper.py
+++ libs/AudioHelper.py
@@ -3,7 +3,7 @@
 ***REMOVED***
 
 
-def split_audio(audio_file: str, length: int, start: 0):
+def split_audio(audio_file: str, length=None, start=0):
     """截取音频文件
         新文件会保存在相同目录, 新文件名： 原文件名_bak.原文件后缀
     
@@ -13,10 +13,15 @@ def split_audio(audio_file: str, length: int, start: 0):
         start: 截取音频的开始位置
     """
     mp3 = AudioSegment.from_file(audio_file)
-    new_audio = mp3[start: start + length] 
+    if not length:
+        new_audio = mp3[start:]
+    elif not start:
+        new_audio = mp3[0:length]
+    else:
+        new_audio = mp3[start: start + length]
     file_name, file_extension =  os.path.splitext(audio_file)
     format = file_extension.replace(".", "")
     new_audio.export(file_name + "_bak" + file_extension, format=format)
     
 ***REMOVED***
-    split_audio("resources/ShengYin/打他.mp3", 1500, 1500)
+    split_audio("水浒传/第五回/鲁智深闹事/鲁达你等着我告诉方丈制裁你.mp3", start=800)
